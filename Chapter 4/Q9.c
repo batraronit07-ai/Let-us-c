277a6a6cfa917245e8b5e9eff9011746a227052b
batraronit07-ai <batra.ronit07@gmail.com>
2025-09-03 16:55:49 +0530

    Chapter 4

--- Chapter 4/Q9.c
+++ Chapter 4/Q9.c
@@ -1,18 +1,42 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main() {
+int main()
+{
 
-    int number1, number2;
-    char another;
+    int number1, number2, number3, count, range;
 
-    do 
+    printf("How many numbers do you want to compare: ");
+    scanf("%d", &count);
+
+    printf("Enter the first number: ");
+    scanf("%d", &number1);
+
+    for (int i = 1; i < count; i++)
     {
-        printf("Enter a number: ");
-        scanf("%d", &number1);
-        printf("Want to print another number y/n: ");
+        printf("Enter the %d number: ", i + 1 );
         scanf("%d", &number2);
 
-        if 
-        
+        if (number1 > number2)
+        {
+            number3 = number2;
+        }
+
+        else if (number2 > number1)
+        {
+            number1 = number2;
+        }
+
+        else if (number2 < number3)
+        {
+            number3 = number2;
+        }
     }
+
+    printf("The highest number is %d\n", number1);
+    printf("The lowest number is %d\n", number3);
+
+    range = number1 - number3;
+
+    printf("The range of the set of questions is %d", range);
+    return 0;
 }
